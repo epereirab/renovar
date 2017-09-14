6312c6cc70ac7a82f94ac9ccaf5a3157a4ad4093
Carlos Suazo <csuazo@gmail.com>
2017-09-14 18:53:15 -0300

    Agrega bash para lanzar procesos en paralelo

---
+++ renovar/corre.sh
@@ -0,0 +1,13 @@
+ARG1=${1:-1} #Numero de corridas
+ARG2=${2:-1} #Procesos en paralelo
+
+echo MONTE CARLO - PROCESO RENOVAR 2
+echo Corridas          = $ARG1
+echo Procesos paralelos= $ARG2
+for i in `seq 1 $ARG1`; do
+   echo Corriendo simulacion $i
+   (python main.py > salida.out) &
+   if (( $i % $ARG2 == 0 ));
+      then wait;
+   fi
+done
