9e3bc445daad035e34e4e13cc34e5ee188cb84eb
U-VPUENTEM\Victor Puente <victorpuentem@gmail.com>
2017-09-26 11:51:21 -0300

    modifica itera_precios.sh y datos de entrada

--- renovar/itera_precios.sh
+++ renovar/itera_precios.sh
@@ -15,7 +15,7 @@ mv $dirdata/data_generadores.csv $dirdata/original_data_generadores.csv
 for precio in `seq $ARG4 $ARG6 $ARG5`; do
    # Cambia precio de proyecto
    echo " -------------- SENSIBILIDAD PRECIO = " $precio "----------------"
-   awk -F ',' -v gen="$ARG3" -v pre="$precio" -v OFS=',' '$1 { if ($1==gen) $8=pre; print}' $dirdata/original_data_generadores.csv > $dirdata/data_generadores.csv
+   awk -F ',' -v gen="$ARG3" -v pre="$precio" -v OFS=',' '$1 { if ($1==gen) $10=pre; print}' $dirdata/original_data_generadores.csv > $dirdata/data_generadores.csv
    # Corre simulacion
    ./corre.sh $ARG1 $ARG2
    # Guarda sensibilidad con precio
